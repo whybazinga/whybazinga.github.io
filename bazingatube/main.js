3f455daabcaaba29f1bbb0c4bcf5fe3e73750c17
Alex Muharski <aom9@mail.ru>
2017-11-18 22:48:32 +0300

    still fixing

--- bazingatube/main.js
+++ bazingatube/main.js
@@ -1,3 +1,31 @@
+function onClientLoad() {
+    gapi.client.setApiKey('AIzaSyCO0E_90cr2cmQVT3lL5vvbE-CcIXi2P5c');
+    gapi.client.load('youtube', 'v3', function () {
+        alert("2");
+    });
+}
+
+function search() {
+    // Use the JavaScript client library to create a search.list() API call.
+    alert("search");
+    let request = gapi.client.youtube.search.list({
+        part: 'snippet',
+        q: searchQuery,
+        maxResults: 3,
+    });
+
+    request.execute(function (response) {
+        alert("onSearchResponse");
+        showResponse(response);
+    });
+}
+
+
+let video = document.getElementsByClassName("video")[0];
+
+//===========================================================================
+
+
 let input = document.getElementById("search-box");
 let button = document.getElementsByClassName("button")[0];
 
@@ -5,7 +33,6 @@ let searchQuery = "";
 
 function buttonClick() {
     searchQuery = input.value;
-    alert("!");
     search();
 }
 
@@ -27,44 +54,3 @@ function buttonView() {
 button.addEventListener("click", buttonClick);
 input.addEventListener("click", inputClick);
 input.addEventListener("input", buttonView);
-
-//===========================================================================
-
-function showResponse(response) {
-    var responseString = JSON.stringify(response, '', 2);
-    video += responseString;
-}
-
-function onClientLoad() {
-    gapi.client.setApiKey('AIzaSyCO0E_90cr2cmQVT3lL5vvbE-CcIXi2P5c');
-    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
-}
-
-function onYouTubeApiLoad() {
-    alert("load2");
-}
-
-function search() {
-    // Use the JavaScript client library to create a search.list() API call.
-    alert("search");
-    let request = gapi.client.youtube.search.list({
-        part: 'snippet',
-        q: searchQuery,
-    });
-
-    // Send the request to the API server,
-    // and invoke onSearchRepsonse() with the response.
-    request.execute(onSearchResponse);
-}
-
-// Called automatically with the response of the YouTube API request.
-function onSearchResponse(response) {
-    alert("onSearchResponse");
-    showResponse(response);
-}
-
-
-
-
-let video = document.getElementsByClassName("video")[0];
-
