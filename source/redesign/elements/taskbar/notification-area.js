c480b8e979f65cf996eb40e16fc0befd03df7d68
whybazinga <muharskialex@gmail.com>
2024-12-26 19:09:21 +0100

    Add simple time changing and in-code program notifications displaying for notification area. Add speaker and mouse icon resources and add them as default program notifications

---
+++ source/redesign/elements/taskbar/notification-area.js
@@ -0,0 +1,45 @@
+const notificationAreaElement = document.querySelector("#taskbar__notification-area");
+
+const notificationAreaTimeElement = document.querySelector("#taskbar__notification-area__time");
+
+const navigationArea = {
+    addProgramNotification: (iconPath) => {
+        if (!notificationAreaElement || !notificationAreaTimeElement)
+        {
+            console.error("No notificationAreaElement or notificationAreaTimeElement!");
+            return;
+        }
+    
+        const newIcon = new Image(16, 16);
+        newIcon.src = iconPath;
+        newIcon.classList.add("taskbar__notification-area__program-icon");
+    
+        notificationAreaElement.insertBefore(newIcon, notificationAreaTimeElement);
+    },
+
+}
+
+
+// ============================ Private ================================
+
+const _setupTaskbarNotificationsAreaTime = () => {
+    if (!notificationAreaTimeElement) {
+        console.error("No notificationAreaTimeElement!");
+    }
+
+    const updateTime = () => {
+        notificationAreaTimeElement.innerHTML = new Date().toLocaleString(
+            'en-US', 
+            {
+                hour: 'numeric',
+                minute: 'numeric',
+                hour12: true
+            }
+        );
+    };
+
+    setInterval(updateTime, 1000 * 60);
+    updateTime();
+}
+
+_setupTaskbarNotificationsAreaTime();
---
+++ source/redesign/index.js
@@ -0,0 +1,5 @@
+// taskbar
+{
+    navigationArea.addProgramNotification("public/resources/windows-98-mouse-icon-16x16.png");
+    navigationArea.addProgramNotification("public/resources/windows-98-loudspeaker_muted-icon-16x16.png");
+}
